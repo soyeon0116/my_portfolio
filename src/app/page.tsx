5810a443208538dce557e9dc12e3bf5773e0b64f
김소연 <ksy9252@naver.com>
2025-09-29 16:58:23 +0900

    feat : main 페이지 왼쪽 영역 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,44 @@
-import Image from "next/image";
+import Image from 'next/image';
+import { FaDownload } from 'react-icons/fa6';
+import { SiVelog } from 'react-icons/si';
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+    <>
+      <div className='flex-1 border-1 p-2 flex flex-col text-center items-center justify-around'>
+        <div className='w-[100px] h-[100px] overflow-hidden rounded-full'>
+          <Image
+            src='/images/myPhoto.jpg'
+            alt='김소연의 증명사진'
+            width={100}
+            height={100}
+          />
+        </div>
+        <h1 className='my-2'>
+          DX를 위해 고민하는 개발자 <br /> 김소연입니다.
+        </h1>
+        <div className='my-2 text-left border-1 border-gray-300 rounded-sm px-2 py-1 w-[300px]'>
+          <p>연락처 : 010-4240-0116</p>
+          <p>e-mail : ksy9252@naver.com</p>
+        </div>
+        <div className='flex md:flex-col gap-2 flex-row'>
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='/assets/resume.pdf'
+            download
+            className='border-1 border-gray-300 rounded-sm py-1 px-2 flex gap-2 items-center cursor-pointer hover:bg-gray-300'
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            이력서 다운로드 <FaDownload />
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://velog.io/@soyeon0116/posts'
+            className='border-1 border-gray-300 rounded-sm py-1 px-2 flex gap-2 items-center cursor-pointer hover:bg-gray-300'
           >
-            Read our docs
+            Velog 둘러보기
+            <SiVelog />
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <div className='flex-2 border-1 p-2'></div>
+    </>
   );
 }
