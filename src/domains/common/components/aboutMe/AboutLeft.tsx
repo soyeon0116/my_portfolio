abb342c66175fc22c20a488de0d9c3a4f1785739
김소연 <ksy9252@naver.com>
2025-10-02 17:41:59 +0900

    style : 소개 부분 스타일 수정

--- src/domains/common/components/aboutMe/AboutLeft.tsx
+++ src/domains/common/components/aboutMe/AboutLeft.tsx
@@ -14,7 +14,7 @@ export default function AboutLeft() {
       <h1 className='my-2'>
         DX를 위해 고민하는 개발자 <br /> 김소연입니다.
       </h1>
-      <div className='my-2 text-left border-1 border-gray-300 rounded-sm px-2 py-1 w-[300px]'>
+      <div className='my-2 text-left border-1 border-gray-300 rounded-sm px-2 py-1'>
         <p>연락처 : 010-4240-0116</p>
         <p>e-mail : ksy9252@naver.com</p>
       </div>
