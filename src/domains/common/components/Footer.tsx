216de6c58b6ccde5ffaedd4076a7e73b8f08c9f4
김소연 <ksy9252@naver.com>
2025-10-09 15:30:38 +0900

    docs : footer내용 변경

--- src/domains/common/components/Footer.tsx
+++ src/domains/common/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className='h-14 bg-gray-700 text-center flex flex-col justify-center'>
-      <p className='text-white'>© 2025.10.02</p>
+      <p className='text-white'>© SOYEON</p>
     </div>
   );
 }
