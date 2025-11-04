9a24d3302aae729b5290da230da1fe7f52ca18e7
김소연 <ksy9252@naver.com>
2025-11-04 10:27:39 +0900

    style : 프로젝트 설명 추가

--- src/domains/common/components/projects/Projects.tsx
+++ src/domains/common/components/projects/Projects.tsx
@@ -6,6 +6,10 @@ export default function Projects() {
     <div id='projects'>
       <section>
         <h1 className='text-4xl font-black text-center mb-2'>Projects</h1>
+        <p className='text-center mb-2 text-gray-500'>
+          * 프로젝트의 이름을 클릭하면 작업 내용의 상세내용을 확인할 수
+          있습니다.
+        </p>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 '>
           {projectList.map((project) => (
             <ProjectCard key={project.id} {...project} />
